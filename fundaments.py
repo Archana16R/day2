107ab4a41e1d573b455df55a289ed2e64d41e8e9
Archana16R <archanaramesha16@gmail.com>
2026-02-03 12:56:37 +0530

    day2_ass

---
+++ fundaments.py
@@ -0,0 +1,32 @@
+#variables
+name = "Acchu"
+age = 22
+print(name)
+print(age)
+print()
+#datatypes
+name="Acchu"
+age=22
+height=5.6
+flag=True
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(flag))
+print()
+#type casting
+a = "100"
+b = int(a)
+c = float(a)
+print(b)
+print(c)
+print()
+#operators
+num1 = 10
+num2 = 3
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+print(num1 ** num2)
